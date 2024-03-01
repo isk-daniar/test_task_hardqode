40deef50c707fb6d716dbb486cd9242b05e9318d
Isk-Daniar <109099588+Isk-Daniar@users.noreply.github.com>
2024-03-01 14:56:28 +0500

    update readme

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Тестовое задание Backend/Django
 
+### Построение архитектуры
+
+1. Создан модель Product с полями: author, name, start_date, cost, min_students и max_students
+2. Для определения доступа пользователя к продукту, создан модель ProductAccess с полями user и product. Это позволит нам связать пользователя с продуктом, к которому он имеет доступ.
+3. Создан модель Lesson с полями: product , name и video_link
+4. Создан модель Group с полями: product , name и students
+
 ### Схема таблиц база данных
 <img src="courses_project/media/courses.jpg">
--- courses_project/config/settings.py
+++ courses_project/config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2!dkhlow9k7sf-m%w&k+#yk7ia!e*6_$nbhz!x!o279983pbo)'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get("DJANGO_ALLOWED_HOSTS")]
 
 
 # Application definition
